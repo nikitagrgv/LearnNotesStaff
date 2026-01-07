7574f30fc147f2fdd4537b563ecb57e1f7f85ae7
Nikita Grigorev <pettyaelse@gmail.com>
2026-01-08 02:38:34 +0700

    w

--- LearnNotesStaff/MainWindow.xaml.cs
+++ LearnNotesStaff/MainWindow.xaml.cs
@@ -1,23 +1,84 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Melanchall.DryWetMidi.Multimedia;
+using Melanchall.DryWetMidi.Core;
+using System;
 
-namespace LearnNotesStaff;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace LearnNotesStaff
 {
-	public MainWindow()
+	public partial class MainWindow : Window
 	{
-		InitializeComponent();
+		private InputDevice _inputDevice;
+		private int _targetMidiNote;
+		private Random _random = new Random();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			SetupMidi();
+			GenerateNewNote();
+		}
+
+		private void SetupMidi()
+		{
+			try
+			{
+				InputDevice device = InputDevice.GetByIndex(0);
+				_inputDevice = device;
+				_inputDevice.EventReceived += OnMidiEventReceived;
+				_inputDevice.StartEventsListening();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Could not find MIDI device: {ex.Message}");
+			}
+		}
+
+		private void GenerateNewNote()
+		{
+			// Range 60 (Middle C) to 72 (C5)
+			_targetMidiNote = _random.Next(60, 73);
+
+			// We use Dispatcher because this might be called from the MIDI thread
+			Dispatcher.Invoke(() =>
+			{
+				NoteDisplay.Text = $"Play MIDI Note: {_targetMidiNote}";
+				StatusDisplay.Text = "Waiting for input...";
+				StatusDisplay.Foreground = System.Windows.Media.Brushes.Gray;
+			});
+		}
+
+		private void OnMidiEventReceived(object? sender, MidiEventReceivedEventArgs e)
+		{
+			if (e.Event is NoteOnEvent noteOn)
+			{
+				int playedNote = noteOn.NoteNumber;
+
+				// IMPORTANT: UI updates must happen on the UI Thread
+				Dispatcher.Invoke(() => { CheckNote(playedNote); });
+			}
+		}
+
+		private void CheckNote(int playedNote)
+		{
+			if (playedNote == _targetMidiNote)
+			{
+				StatusDisplay.Text = "Correct! Well done.";
+				StatusDisplay.Foreground = System.Windows.Media.Brushes.Green;
+
+				// Wait a moment then show next note (simplified here)
+				GenerateNewNote();
+			}
+			else
+			{
+				StatusDisplay.Text = $"Wrong! You played {playedNote}. Try again!";
+				StatusDisplay.Foreground = System.Windows.Media.Brushes.Red;
+			}
+		}
+
+		protected override void OnClosed(EventArgs e)
+		{
+			_inputDevice?.Dispose();
+			base.OnClosed(e);
+		}
 	}
 }
